08a5606cbf8b604ca0849b58db5126da571ef229
Joe Dallago <jd.dallago@gmail.com>
2014-03-17 23:53:13 +0000

    Added a newline to the string.

--- python_example/views.py
+++ python_example/views.py
@@ -4,4 +4,4 @@ from pyramid.view import view_config
 @view_config(route_name='index',
              renderer='string')
 def hello_world(request):
-    return 'hello world'
+    return 'hello world\n'
