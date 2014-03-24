8ff2e350fa9ed50f7a12729ee099fa8030f1048e
vagrant <vagrant@packer-virtualbox.(none)>
2014-03-24 06:40:22 +0000

    Bump.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ entry_points = """
 """
 
 setup(name='python_example',
-      version='0.0.0',
+      version='0.0.1',
       description='Flynn python example.',
       long_description=README,
       packages=find_packages(),
